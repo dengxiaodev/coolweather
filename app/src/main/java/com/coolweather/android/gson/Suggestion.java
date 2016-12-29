b156bb7325b1300b6aa1d460996e6e82dda49c21
Deng <deng@gmail.com>
2016-12-29 21:42:05 +0800

    更改了后台刷新时间，添加了一些生活建议。

--- app/src/main/java/com/coolweather/android/gson/Suggestion.java
+++ app/src/main/java/com/coolweather/android/gson/Suggestion.java
@@ -10,11 +10,44 @@ public class Suggestion {
     @SerializedName("comf")
     public Comfort comfort;
 
-    @SerializedName("cw")
-    public CarWash carWash;
+    @SerializedName("drsg")
+    public Dress dress;
+
+    @SerializedName("flu")
+    public Cold cold;
+
     public Sport sport;
 
+    @SerializedName("trav")
+    public Travel travel;
+
+    @SerializedName("cw")
+    public CarWash carWash;
+
+    @SerializedName("uv")
+    public Ultraviolet ultraviolet;
+
     public class Comfort {
+        @SerializedName("txt")
+        public String info;
+    }
+
+    public class Dress {
+        @SerializedName("txt")
+        public String info;
+    }
+
+    public class Cold {
+        @SerializedName("txt")
+        public String info;
+    }
+
+    public class Sport {
+        @SerializedName("txt")
+        public String info;
+    }
+
+    public class Travel {
         @SerializedName("txt")
         public String info;
     }
@@ -24,7 +57,7 @@ public class Suggestion {
         public String info;
     }
 
-    public class Sport {
+    public class Ultraviolet {
         @SerializedName("txt")
         public String info;
     }
--- app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -31,7 +31,7 @@ public class AutoUpdateService extends Service {
         updateWeather();
         updateBingPic();
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        int anHour = 8 * 60 * 60 * 1000;//这是8小时的毫秒数
+        int anHour = 4 * 60 * 60 * 1000;//这是4小时的毫秒数
         long triggerAtTime = SystemClock.elapsedRealtime()+anHour;
         Intent i = new Intent(this,AutoUpdateService.class);
         PendingIntent pi = PendingIntent.getService(this,0,i,0);
